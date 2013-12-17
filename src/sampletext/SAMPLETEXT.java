167b561e11ee2d5ff05b964d307674abefc828d4
Poseidon <Poseidon@Poseidon-PC.gateway.2wire.net>
2013-12-17 16:45:07 -0500

    furst cimut get quickscoped noob

---
+++ src/sampletext/SAMPLETEXT.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sampletext;
+
+/**
+ *
+ * @author Poseidon
+ */
+public class SAMPLETEXT {
+    public static void printDoritos() {
+        try { 
+        System.out.println("Sample text v420 : Sponsored by Doritos, Machinima, Mountain Dew and the Holocaust (trademark)");
+           //get fuken owned nub
+        }catch(Exception e ) {
+            System.err.println("get feken owneed screb");
+        }
+        }
+}
